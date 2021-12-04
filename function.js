61c25aa34c57df5b391d9387dd37f244bdc2b89e
Alex Merren <alexandermerren@gmail.com>
2021-12-04 13:43:21 +0000

    Added new page

--- function.js
+++ function.js
@@ -1,4 +1,5 @@
 function displayAgeAndCalendar() {
+    // Perform input validation for age input.
     let dateEl = document.getElementById("header__dateInput");
     let birthday = new Date(dateEl.value);
     if (isNaN(birthday)) {
@@ -33,4 +34,25 @@ function displayCalendar(birthday) {
         }
         document.getElementById("after__weeks-container").appendChild(weekDiv);
     }
+}
+
+function displayAgeOnAgePage() {
+    // Get the birthday from the input provided.
+    let dateEl = document.getElementById("age__dateInput");
+    let birthday = new Date(dateEl.value);
+    if (isNaN(birthday)) {
+        return;
+    }
+    
+    // Hide the input and show the age display.
+    document.getElementById("age").style.display = "none";
+    document.getElementById("age__display__parent").style.display = "flex";
+    
+    // Increment the age every 50 milliseconds.
+    let ageEl = document.getElementById("age__display");
+    setInterval(() => {
+        let time = dayjs().diff(dayjs(birthday), 'year', true);
+        ageEl.innerText = time.toString().substring(0, 12);
+        document.title = time.toString().substring(0, 12);
+    }, 50);
 }
