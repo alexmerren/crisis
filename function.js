61bf56c94c14c582203fd982566a867b420f63dc
Alex Merren <alexandermerren@gmail.com>
2022-03-28 19:44:32 +0100

    changes

--- function.js
+++ function.js
@@ -1,113 +1,101 @@
-// Check if cookies are set, and load the calendar and timer if it is.
+var queryString = window.location.search;
+var urlParams = new URLSearchParams(queryString);
+
 window.addEventListener("load", () => {
-    let birthdayCookie = getCookie('birthday');
-    if (birthdayCookie) {
-        Index_DisplayAgeAndCalendar(birthdayCookie);
-    }
+  let birthday = urlParams.get('birthday');
+  if (birthday == null) { return; }
+  if (window.location.href.match('index') != null) {
+    Index_DisplayAgeAndCalendar(birthday);
+  }
+  if (window.location.href.match('age') != null) {
+    Age_DisplayAge(birthday);
+  }
 });
 
-function Index_DisplayAgeAndCalendar(optionalBirthday) {
-    let birthday;
+function Index_DisplayAgeAndCalendar(birthday) {
+  if (inputValidation("header__dateInput") != null) {
+    birthday = inputValidation("header__dateInput");
+  }
 
-    if (optionalBirthday != null) {
-        birthday = optionalBirthday;
-    }
+  if (urlParams.get("birthday") != birthday) {
+    birthday = dayjs(birthday, "YYYY MM DD");
+    urlParams.set("birthday", birthday);
+    document.location.search = urlParams.toString();
+  }
 
-    if (inputValidation("header__dateInput") != null) {
-        birthday = inputValidation("header__dateInput");
-        setCookie('birthday', birthday);
-    }
-
-    displayAge(birthday, "after__age");
-    displayCalendar(birthday, "after", "after__weeks-container");
+  displayAge(birthday, "after__age");
+  displayLink(birthday, "after__link");
+  displayCalendar(birthday, "after", "after__weeks-container");
 }
 
-function Age_DisplayAge(optionalBirthday) {
-    let birthday;
+function Age_DisplayAge(birthday) {
+  if (inputValidation("age__dateInput") != null) {
+    birthday = inputValidation("age__dateInput");
+  }
 
-    if (optionalBirthday != null) {
-        birthday = optionalBirthday;
-    }
+  // Hide the input and show the age display
+  document.getElementById("age").style.display = "none";
+  document.getElementById("age__display__parent").style.display = "flex";
+  
+  if (urlParams.get("birthday") != birthday) {
+    birthday = dayjs(birthday, "YYYY MM DD");
+    urlParams.set("birthday", birthday);
+    document.location.search = urlParams.toString();
+  }
 
-    if (inputValidation("age__dateInput") != null) {
-        birthday = inputValidation("age__dateInput");
-        setCookie('birthday', birthday);
-    }
-
-    // Hide the input and show the age display
-    document.getElementById("age").style.display = "none";
-    document.getElementById("age__display__parent").style.display = "flex";
-    
-    // Display the gae in the age__display element
-    displayAge(birthday, "age__display");
+  // Display the age__display element
+  displayAge(birthday, "age__display");
 }
 
 function inputValidation(elementName) {
-    let dateEl = document.getElementById(elementName);
-    if (!dateEl) {
-        return null;
-    }
+  let dateEl = document.getElementById(elementName);
+  if (!dateEl) {
+      return null;
+  }
 
-    let birthday = new Date(dateEl.value);
-    if (isNaN(birthday)) {
-        return null;
-    }
-    return birthday;
+  let birthday = new Date(dateEl.value);
+  if (isNaN(birthday)) {
+      return null;
+  }
+  return birthday;
 }
 
 function displayAge(birthday, elementName) {
-    // Get the reference for the specified element, and constantly refresh the number
-    let ageEl = document.getElementById(elementName);
-    setInterval(() => {
-        let time = dayjs().diff(dayjs(birthday), 'year', true);
-        ageEl.innerText = time.toString().substring(0, 12);
-    }, 50);
+  // Get the reference for the specified element, and constantly refresh the number
+  let ageEl = document.getElementById(elementName);
+  setInterval(() => {
+      let time = dayjs().diff(dayjs(birthday), 'year', true);
+      ageEl.innerText = time.toString().substring(0, 12);
+  }, 50);
 }
 
-function displayCalendar(birthday, parentElement, element) {
-    document.getElementById(parentElement).style.display = "flex";
-    let currentAge = dayjs().diff(dayjs(birthday), 'year', true);
-    let currentAgeInWeeks = currentAge * 52;
-    let totalWeeksInLife = 71 * 52;
-    for (let i = 0; i < totalWeeksInLife; ++i) {
-        if (i % 52 == 0) {
-            document.getElementById(element).appendChild(document.createElement("br"));
-            let number = document.createElement("p");
-            number.innerHTML = i / 52;
-            document.getElementById(element).appendChild(number);
-        }
-
-        let weekDiv = document.createElement("div");
-        if (i < currentAgeInWeeks) {
-            weekDiv.style.backgroundColor = "black";
-        }
-
-        if (i === Math.floor(currentAgeInWeeks)) {
-            weekDiv.style.backgroundColor = "#E26D5A";
-            weekDiv.style.border = "1px #E26D5A solid";
-        }
-
-        document.getElementById(element).appendChild(weekDiv);
+function displayCalendar(birthday, parentElement, calendarElement) {
+  document.getElementById(parentElement).style.display = "flex";
+  let currentAge = dayjs().diff(dayjs(birthday), 'year', true);
+  let currentAgeInWeeks = currentAge * 52;
+  let totalWeeksInLife = 82 * 52;
+  for (let i = 0; i < totalWeeksInLife; ++i) {
+    // Create the number at the left of the row, and append a break so that it
+    // goes to the next line.
+    if (i % 52 == 0) {
+      document.getElementById(calendarElement).appendChild(document.createElement("br"));
+      let number = document.createElement("p");
+      number.innerHTML = i / 52;
+      document.getElementById(calendarElement).appendChild(number);
     }
-}
 
-// These cookie functions are taken from https://stackoverflow.com/questions/14573223/set-cookie-and-get-cookie-with-javascript
-function getCookie(name) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(';');
-    for(var i=0;i < ca.length;i++) {
-        var c = ca[i];
-        while (c.charAt(0)==' ') c = c.substring(1,c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    // Create the dot and decide which background color it should have.
+    let weekDiv = document.createElement("div");
+    if (i < currentAgeInWeeks) {
+      weekDiv.style.backgroundColor = "black";
     }
-    return null;
+    if (i === Math.floor(currentAgeInWeeks)) {
+      weekDiv.style.backgroundColor = "#E26D5A";
+      weekDiv.style.border = "1px #E26D5A solid";
+    }
+
+    document.getElementById(calendarElement).appendChild(weekDiv);
+  }
 }
 
-function setCookie(name, value) {
-    var expires = "";
-    document.cookie = name + "=" + (value || "")  + expires + "; path=/; SameSite=None; Secure";
-}
-
-function eraseCookie(name) {
-    document.cookie = name +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
-}
+function displayLink(birthday, element) {}
